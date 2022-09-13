9237c0a79517314dc0512688863b5e46b3bb434f
Luca Feigl <lfeigl.dev@gmail.com>
2022-09-13 22:43:34 +0200

    Add env mode indicator to extension name & get version from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { crx } from '@crxjs/vite-plugin';
-import manifest from './manifest.json';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+import manifestFile from './manifest.json';
+import { version } from './package.json';
+
+const manifest = defineManifest((env) => ({
+  name: `UnyCon ${env.mode === 'development' ? '🚧🚧🚧' : '🦄'}`,
+  version,
+  ...manifestFile,
+}));
 
 // https://vitejs.dev/config/
 export default defineConfig({
